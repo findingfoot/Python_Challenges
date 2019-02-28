19eef68aadedfd2813a0672940bf9d4718b16238
prashant <prashant.x.kumar@ericsson.com>
2019-02-28 13:37:18 +0100

    maximal square working

---
+++ Day1.py
@@ -0,0 +1,7 @@
+def count_char(thread, char):
+    count  = 0
+    for i in range(len(thread)):
+        if char thread:
+            count +=1
+
+    print(count)
---
+++ maximal_square.py
@@ -0,0 +1,24 @@
+def MaximalSquare(strArr):
+    squarevalue = []
+    base_value = 1
+    for s in strArr:
+        row = []
+        for i in s:
+            if i == '0':
+                row.append(0)
+            else:
+                row.append(1)
+        squarevalue.append(row)
+
+    for p in range(1, len(squarevalue)):
+        for q in range(1, len(squarevalue[p])):
+            if squarevalue[p][q] > 0:
+                squarevalue[p][q] = 1 + min(squarevalue[p - 1][q], squarevalue[p][q - 1], squarevalue[p - 1][q - 1])
+                if squarevalue[p][q] > base_value:
+                    base_value = squarevalue[p][q]
+
+    return base_value * base_value
+
+
+# keep this function call here
+print (MaximalSquare(input()))
